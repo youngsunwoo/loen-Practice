8d1b253bb2bd21963c351b20b522957f1ce081d1
youngsunWoo <32916757+youngsunWoo@users.noreply.github.com>
2017-11-10 15:38:25 +0900

    Update 테이블생성쿼리.sql

--- 테이블생성쿼리.sql
+++ 테이블생성쿼리.sql
@@ -1,3 +1,23 @@
+CREATE TABLE `User` (
+  `user_id` varchar(50) NOT NULL,
+  `user_pw` varchar(50) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `age` int(11) NOT NULL,
+  `gender` char(1) NOT NULL,
+  `point` int(11) NOT NULL DEFAULT '0',
+  `Auth` varchar(1) DEFAULT '0',
+  PRIMARY KEY (`user_id`),
+  KEY `IND_U_NAME` (`user_name`)
+
+  CREATE TABLE `Product` (
+  `product_code` char(5) NOT NULL,
+  `product_name` varchar(50) NOT NULL,
+  `price` int(11) NOT NULL,
+  `discription1` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`product_code`),
+  KEY `IND_U_NAME` (`price`)
+)
+
 CREATE TABLE `Benefit` (
   `promo_type` char(1) NOT NULL,
   `benefit_code` char(2) NOT NULL,
@@ -6,30 +26,6 @@ CREATE TABLE `Benefit` (
   `unit` varchar(10) NOT NULL,
   PRIMARY KEY (`promo_type`,`benefit_code`,`goal_cnt`),
   KEY `IND_U_NAME` (`goal_cnt`)
-) 
-
-
-CREATE TABLE `JoinList` (
-  `promo_id` char(10) NOT NULL,
-  `join_user_id` varchar(50) NOT NULL,
-  `purchase_id` varchar(10) NOT NULL,
-  `join_date` datetime NOT NULL,
-  `join_from` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`promo_id`,`join_user_id`),
-  KEY `fk_JoinList_Promotion1_idx` (`promo_id`),
-  KEY `fk_JoinList_User1_idx` (`join_user_id`),
-  KEY `fk_JoinList_Purchase1_idx` (`purchase_id`),
-  CONSTRAINT `fk_JoinList_Promotion1` FOREIGN KEY (`promo_id`) REFERENCES `Promotion` (`promo_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_JoinList_Purchase1` FOREIGN KEY (`purchase_id`) REFERENCES `Purchase` (`purchase_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_JoinList_User1` FOREIGN KEY (`join_user_id`) REFERENCES `User` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) 
-
-CREATE TABLE `Product` (
-  `product_code` char(5) NOT NULL,
-  `product_name` varchar(50) NOT NULL,
-  `price` int(11) NOT NULL,
-  PRIMARY KEY (`product_code`),
-  KEY `IND_U_NAME` (`price`)
 ) 
 
 CREATE TABLE `Promotion` (
@@ -51,12 +47,9 @@ CREATE TABLE `Promotion` (
   CONSTRAINT `fk_Promotion_Benefit1` FOREIGN KEY (`promo_type`, `benefit_code`) REFERENCES `Benefit` (`promo_type`, `benefit_code`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_Promotion_Product1` FOREIGN KEY (`product_code`) REFERENCES `Product` (`product_code`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_Promotion_User1` FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-  )
+)
 
-
-
-
-  CREATE TABLE `Purchase` (
+CREATE TABLE `Purchase` (
   `purchase_id` char(10) NOT NULL,
   `purchase_date` datetime NOT NULL,
   `user_id` varchar(50) NOT NULL,
@@ -70,13 +63,19 @@ CREATE TABLE `Promotion` (
   CONSTRAINT `fk_Purchase_User1` FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) 
 
-CREATE TABLE `User` (
-  `user_id` varchar(50) NOT NULL,
-  `user_pw` varchar(50) NOT NULL,
-  `user_name` varchar(50) NOT NULL,
-  `age` int(11) NOT NULL,
-  `gender` char(1) NOT NULL,
-  `point` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`user_id`),
-  KEY `IND_U_NAME` (`user_name`)
+
+
+CREATE TABLE `Participate` (
+  `promo_id` char(10) NOT NULL,
+  `join_user_id` varchar(50) NOT NULL,
+  `purchase_id` varchar(10) NOT NULL,
+  `join_date` datetime NOT NULL,
+  `join_from` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`promo_id`,`join_user_id`),
+  KEY `fk_JoinList_Promotion1_idx` (`promo_id`),
+  KEY `fk_JoinList_User1_idx` (`join_user_id`),
+  KEY `fk_JoinList_Purchase1_idx` (`purchase_id`),
+  CONSTRAINT `fk_JoinList_Promotion1` FOREIGN KEY (`promo_id`) REFERENCES `Promotion` (`promo_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_JoinList_Purchase1` FOREIGN KEY (`purchase_id`) REFERENCES `Purchase` (`purchase_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_JoinList_User1` FOREIGN KEY (`join_user_id`) REFERENCES `User` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 )
